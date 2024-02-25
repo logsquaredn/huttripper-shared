e1c44cd11d0f68cdc9380b52867ec4ebbd71dfb1
evan <logsdonevan1@gmail.com>
2024-02-24 20:54:32 -0800

    add s3 path attrs impl

--- src/model/object.rs
+++ src/model/object.rs
@@ -1,13 +1,25 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgRow, Row};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct S3PathAttributes {
     pub sanitized_state: String,
     pub sanitized_system: String,
     pub sanitized_name: String
 }
 
-#[derive(Debug, Serialize)]
+impl S3PathAttributes {
+
+    pub fn map_from(row: PgRow) -> Result<Self, sqlx::Error> {
+        Ok(Self { 
+            sanitized_state: row.try_get("sanitizedstate")?,
+            sanitized_system: row.try_get("sanitizedsystem")?,
+            sanitized_name: row.try_get("sanitizedname")?
+        })
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ImgLinkUpdates {
     pub thumbnail_image: Option<String>,
     pub images: Vec<String>
