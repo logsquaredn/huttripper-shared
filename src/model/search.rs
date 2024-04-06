8a362893d99803edbe80f6b99af3e0b6d3836259
evan <logsdonevan1@gmail.com>
2024-04-05 18:34:54 -0700

    change search rep

--- src/model/search.rs
+++ src/model/search.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct PostgresSearchRepresentation {
+pub struct SearchRepresentation {
     pub name: String,
     pub system: String,
     pub state: String,
-    pub amenities: Vec<String>,
+    pub amenities: String,
     pub sanitizedstate: String,
     pub sanitizedsystem: String
 }
