13d6621131c8553ee42204850e1652cc16ff05fc
evan <logsdonevan1@gmail.com>
2024-04-05 19:24:09 -0700

    add map from for search rep

--- src/model/search.rs
+++ src/model/search.rs
@@ -1,11 +1,29 @@
 use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgRow;
+use sqlx::Row;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct SearchRepresentation {
     pub name: String,
-    pub system: String,
-    pub state: String,
-    pub amenities: String,
     pub sanitizedname: String,
-    pub sanitizedsystem: String
+    pub system: String,
+    pub sanitizedsystem: String,
+    pub state: String,
+    pub amenities: String
+}
+
+impl SearchRepresentation {
+
+    pub fn map_from(row: PgRow) -> Result<Self, sqlx::Error> {
+        let amenities: Vec<String> = row.try_get("amenities")?;
+        amenities.join(" ");
+        Ok(Self {
+            name: row.try_get("name")?,
+            sanitizedname: row.try_get("sanitizedname")?,
+            system: row.try_get("system")?,
+            sanitizedsystem: row.try_get("sanitizedsystem")?,
+            state: row.try_get("state")?,
+            amenities: amenities.join(" ")
+        })
+    }
 }
