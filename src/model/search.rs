e9dcabb0aa60cae3bfec0950304b6cce28a4e37d
evan <logsdonevan1@gmail.com>
2024-04-05 22:34:13 -0700

    add trip report search rep

--- src/model/search.rs
+++ src/model/search.rs
@@ -3,27 +3,47 @@ use sqlx::postgres::PgRow;
 use sqlx::Row;
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct SearchRepresentation {
+pub struct HutSearchRepresentation {
     pub name: String,
-    pub sanitizedname: String,
+    pub sanitized_name: String,
     pub system: String,
-    pub sanitizedsystem: String,
+    pub sanitized_system: String,
     pub state: String,
     pub amenities: String
 }
 
-impl SearchRepresentation {
+impl HutSearchRepresentation {
 
     pub fn map_from(row: PgRow) -> Result<Self, sqlx::Error> {
         let amenities: Vec<String> = row.try_get("amenities")?;
         amenities.join(" ");
         Ok(Self {
             name: row.try_get("name")?,
-            sanitizedname: row.try_get("sanitizedname")?,
+            sanitized_name: row.try_get("sanitizedname")?,
             system: row.try_get("system")?,
-            sanitizedsystem: row.try_get("sanitizedsystem")?,
+            sanitized_system: row.try_get("sanitizedsystem")?,
             state: row.try_get("state")?,
             amenities: amenities.join(" ")
+        })
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TripReportSearchRepresentation {
+    pub sanitized_hut_name: String,
+    pub hut_conditions: String,
+    pub weather_conditions: String,
+    pub riding_conditions: String
+}
+
+impl TripReportSearchRepresentation {
+
+    pub fn map_from(row: PgRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            sanitized_hut_name: row.try_get("sanitizedhutname")?,
+            hut_conditions: row.try_get("hutconditions")?,
+            weather_conditions: row.try_get("weatherconditions")?,
+            riding_conditions: row.try_get("ridingconditions")?
         })
     }
 }
