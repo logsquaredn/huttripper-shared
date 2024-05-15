cf8d3d7d85242bbbaf7b44d1a89ce6df1c017909
evan <logsdonevan1@gmail.com>
2024-05-15 11:48:06 -0700

    add shared trip report

--- src/model.rs
+++ src/model.rs
@@ -2,3 +2,4 @@ pub mod geo;
 pub mod avi;
 pub mod object;
 pub mod search;
+pub mod tripreport;
---
+++ src/model/tripreport.rs
@@ -0,0 +1,33 @@
+use sqlx::Row;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct TripReport {
+    pub id: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub trip_start_date: chrono::NaiveDate,
+    pub trip_end_date: chrono::NaiveDate,
+    pub weather_conditions: Option<String>,
+    pub hut_conditions: String,
+    pub riding_conditions: Option<String>,
+    pub approved: bool,
+    pub image_links: Vec<String>,
+}
+
+impl TripReport {
+
+    pub fn map_from(row: sqlx::postgres::PgRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            id: row.try_get("id")?,
+            first_name: row.try_get("firstname")?,
+            last_name: row.try_get("lastname")?,
+            trip_start_date: row.try_get("tripstartdate")?,
+            trip_end_date: row.try_get("tripenddate")?,
+            weather_conditions: row.try_get("weatherconditions")?,
+            hut_conditions: row.try_get("hutconditions")?,
+            riding_conditions: row.try_get("ridingconditions")?,
+            approved: row.try_get("approved")?,
+            image_links: vec![]
+        })
+    }
+}
